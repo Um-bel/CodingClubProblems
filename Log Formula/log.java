590d744881bb411286eaf19b44ca70e40adc2c01
John Mienko <79326975+Um-bel@users.noreply.github.com>
2021-12-09 11:38:26 -0500

    Create log.java

---
+++ Log Formula/log.java
@@ -0,0 +1,19 @@
+import System; 
+
+class log
+{
+    public static void Main(String[] args) 
+    {
+        int[] arr = new int[10]; 
+
+        for(int y = 0; y < arr.length; y++) {
+            Double x = Math.pow(x, y); 
+
+            Double[] arrSmol = new Double[2]; 
+            arrSmol.push(x); 
+            arrSmol.push(y); 
+
+            arr.push(arrSmol); 
+        }
+    }
+}
