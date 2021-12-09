0f5f118a6d6ec447731661940d7c1c6ee022a865
John Mienko <79326975+Um-bel@users.noreply.github.com>
2021-12-09 10:51:01 -0500

    Add files via upload

---
+++ Fibonacci Problem/fib.java
@@ -0,0 +1,20 @@
+import java.io.Console;
+
+import System; 
+
+class notInesh
+{
+    public static void IneshesEyes(String[] args) 
+    {
+        int num0, num1 = 0; 
+        
+        for(int i=0; i<100; i++) 
+        {
+            int newNum = num0 + num1; 
+            num0 = num1; 
+            num1 = newNum; 
+
+            System.out.print(newNum); 
+        }
+    }
+}
